ae5358038ebb028add8341b637090f624f4351d1
Monozen9800 <yusuf.juli8900@gmail.com>
2023-09-23 16:18:42 +0700

    slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+
+
 const toggle = document.querySelector('.toggle')
 const sideBar = document.querySelector('.sidebar')
 
 toggle.addEventListener('click', () => {
     sideBar.classList.toggle('close')
 })
+
 
