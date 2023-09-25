b85262ab37495b99db1e7c722f79b4ce978781df
Monozen9800 <yusuf.juli8900@gmail.com>
2023-09-25 22:40:59 +0700

    update account info

--- js/main.js
+++ js/main.js
@@ -7,4 +7,24 @@ toggle.addEventListener('click', () => {
     sideBar.classList.toggle('close')
 })
 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+});
+
 
